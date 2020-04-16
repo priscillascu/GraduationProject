de9011eb13587e60b7f5dd27ff8a3ec59025e800
SCUPRISCILLA <941705864@qq.com>
2020-04-16 22:59:06 +0800

    七段S型轨迹规划

--- GraduationProject/SevenTrajectory.m
+++ GraduationProject/SevenTrajectory.m
@@ -63,6 +63,14 @@ lx = 0.9331+sum_v*(-0.2)/0.3464;
 ly = 0.0404+sum_v*(0.2)/0.3464;
 lz = 1.0982+sum_v*(-0.2)/0.3464;
 
+vx = v*(-0.2)/0.3464;
+vy = v*(0.2)/0.3464;
+vz = v*(-0.2)/0.3464;
+
+ax = dv*(-0.2)/0.3464;
+ay = dv*(0.2)/0.3464;
+az = dv*(-0.2)/0.3464;
+
 figure
 subplot(3, 1, 1)
 plot(T, sum_v,'LineWidth',1.5);
@@ -87,24 +95,86 @@ title('加速度变化曲线')
 figure
 subplot(1, 3, 1)
 plot(T, lx,'LineWidth',1.5);
-xlabel('t');
-ylabel('x');
+xlabel('t/s');
+ylabel('{x/m}');
+set(gca,'YLim',[min(x)-(max(x)-min(x))/10 max(x)+(max(x)-min(x))/10]);
+axis square
 grid on;
-title('x轴方向位置变化')
+title('x轴方向位移变化')
 
 subplot(1, 3, 2)
 plot(T, ly,'LineWidth',1.5);
-xlabel('t');
-ylabel('y');
+xlabel('t/s');
+ylabel('{y/m}');
+set(gca,'YLim',[min(y)-(max(y)-min(y))/10 max(y)+(max(y)-min(y))/10]);
+axis square
 grid on;
-title('y轴方向位置变化')
+title('y轴方向位移变化')
 
 subplot(1, 3, 3)
 plot(T, lz,'LineWidth',1.5);
-xlabel('t');
-ylabel('z');
+xlabel('t/s');
+ylabel('{z/m}');
+set(gca,'YLim',[min(z)-(max(z)-min(z))/10 max(z)+(max(z)-min(z))/10]);
+axis square
 grid on;
-title('z轴方向位置变化')
+title('z轴方向位移变化')
+
+figure
+subplot(1, 3, 1)
+plot(T, vx,'LineWidth',1.5);
+xlabel('t/s');
+ylabel('{v_x/m/s}');
+set(gca,'YLim',[min(vx)-(max(vx)-min(vx))/10 max(vx)+(max(vx)-min(vx))/10]);
+axis square
+grid on;
+title('x轴方向速度变化')
+
+subplot(1, 3, 2)
+plot(T, vy,'LineWidth',1.5);
+xlabel('t/s');
+ylabel('{v_y/m/s}');
+set(gca,'YLim',[min(vy)-(max(vy)-min(vy))/10 max(vy)+(max(vy)-min(vy))/10]);
+axis square
+grid on;
+title('y轴方向速度变化')
+
+subplot(1, 3, 3)
+plot(T, vz,'LineWidth',1.5);
+xlabel('t/s');
+ylabel('{v_z/m/s}');
+set(gca,'YLim',[min(vz)-(max(vz)-min(vz))/10 max(vz)+(max(vz)-min(vz))/10]);
+axis square
+grid on;
+title('z轴方向速度变化')
+
+figure
+subplot(1, 3, 1)
+plot(T, ax,'LineWidth',1.5);
+xlabel('t/s');
+ylabel('{a_x/m/s^2}');
+set(gca,'YLim',[min(ax)-(max(ax)-min(ax))/10 max(ax)+(max(ax)-min(ax))/10]);
+axis square
+grid on;
+title('x轴方向加速度变化')
+
+subplot(1, 3, 2)
+plot(T, ay,'LineWidth',1.5);
+xlabel('t/s');
+ylabel('{a_y/m/s^2}');
+set(gca,'YLim',[min(ay)-(max(ay)-min(ay))/10 max(ay)+(max(ay)-min(ay))/10]);
+axis square
+grid on;
+title('y轴方向加速度变化')
+
+subplot(1, 3, 3)
+plot(T, az,'LineWidth',1.5);
+xlabel('t/s');
+ylabel('{a_z/m/s^2}');
+set(gca,'YLim',[min(az)-(max(az)-min(az))/10 max(az)+(max(az)-min(az))/10]);
+axis square
+grid on;
+title('z轴方向加速度变化')
 
 %轨迹点逆运动学解算
 T1 = myFKSolver([0; 0; 0; 0; 0; 0]);   %起点位姿矩阵
@@ -123,44 +193,56 @@ end
 figure
 subplot(2, 3, 1)
 plot(T, qc(1, :),'LineWidth',1.5);
-xlabel('t');
-ylabel('q1');
+xlabel('t/s');
+ylabel('{q_1/rad}');
+set(gca,'YLim',[min(qc(1, :))-(max(qc(1, :))-min(qc(1, :)))/10 max(qc(1, :))+(max(qc(1, :))-min(qc(1, :)))/10]);
+axis square
 grid on;
-title('第1关节关节角变化')
+title('第1关节关节角轨迹曲线')
 
 subplot(2, 3, 2)
 plot(T, qc(2, :),'LineWidth',1.5);
-xlabel('t');
-ylabel('q2');
+xlabel('t/s');
+ylabel('{q_2/rad}');
+set(gca,'YLim',[min(qc(2, :))-(max(qc(2, :))-min(qc(2, :)))/10 max(qc(2, :))+(max(qc(2, :))-min(qc(2, :)))/10]);
+axis square
 grid on;
-title('第2关节关节角变化')
+title('第2关节关节角轨迹曲线')
 
 subplot(2, 3, 3)
 plot(T, qc(3, :),'LineWidth',1.5);
-xlabel('t');
-ylabel('q3');
+xlabel('t/s');
+ylabel('{q_3/rad}');
+set(gca,'YLim',[min(qc(3, :))-(max(qc(3, :))-min(qc(3, :)))/10 max(qc(3, :))+(max(qc(3, :))-min(qc(3, :)))/10]);
+axis square
 grid on;
-title('第3关节关节角变化')
+title('第3关节关节角轨迹曲线')
 
 subplot(2, 3, 4)
 plot(T, qc(4, :),'LineWidth',1.5);
-xlabel('t');
-ylabel('q4');
+xlabel('t/s');
+ylabel('{q_4/rad}');
+set(gca,'YLim',[min(qc(4, :))-(max(qc(4, :))-min(qc(4, :)))/10 max(qc(4, :))+(max(qc(4, :))-min(qc(4, :)))/10]);
+axis square
 grid on;
-title('第4关节关节角变化')
+title('第4关节关节角轨迹曲线')
 
 subplot(2, 3, 5)
 plot(T, qc(5, :),'LineWidth',1.5);
-xlabel('t');
-ylabel('q5');
+xlabel('t/s');
+ylabel('{q_5/rad}');
+set(gca,'YLim',[min(qc(5, :))-(max(qc(5, :))-min(qc(5, :)))/10 max(qc(5, :))+(max(qc(5, :))-min(qc(5, :)))/10]);
+axis square
 grid on;
-title('第5关节关节角变化')
+title('第5关节关节角轨迹曲线')
 
 subplot(2, 3, 6)
 plot(T, qc(6, :),'LineWidth',1.5);
-xlabel('t');
-ylabel('q6');
+xlabel('t/s');
+ylabel('{q_6/rad}');
+set(gca,'YLim',[min(qc(6, :))-(max(qc(6, :))-min(qc(6, :)))/10 max(qc(6, :))+(max(qc(6, :))-min(qc(6, :)))/10]);
+axis square
 grid on;
-title('第6关节关节角变化')
+title('第6关节关节角轨迹曲线')
 
 
